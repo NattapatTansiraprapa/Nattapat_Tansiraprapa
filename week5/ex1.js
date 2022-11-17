b639895b3f9f2799fad0f0c11203d344f9fe542e
Nattapat <ucoolzaza123@hotmail.com>
2022-11-17 09:27:06 +0700

    Create ex1.js

---
+++ week5/ex1.js
@@ -0,0 +1,14 @@
+let item = {
+    "A4paper" : 120,
+    "Ruler" : 25,
+    "FlashDrive" : 130
+}
+
+//Mr.Lee
+console.log("Mr.Lee", item.A4paper *5 + item.Ruler * 2)
+
+//Mr.Yue
+console.log("Mr.Yue", item.FlashDrive *2 + item.A4paper * 3)
+
+//Mr.Thee
+console.log("Mr.Thee", item.FlashDrive *2 + item.A4paper * 3 + item.Ruler * 5)
